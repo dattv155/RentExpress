5848ed2d2e6ef8d6bc09e7bacceccce230f43827
TrongDat <trongdat1505@gmail.com>
2021-03-21 17:13:52 +0700

    Fix static in heroku

--- shoppingsite/settings.py
+++ shoppingsite/settings.py
@@ -134,7 +134,7 @@ AUTH_USER_MODEL = 'profiles.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # AUTH_USER_MODEL = 'user.CustomerUser'
 
@@ -142,6 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+# STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
